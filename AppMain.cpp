b893ac70f4f488af5f35043d4818d6ea5b2dad9a
LiYan <robotdev@126.com>
2017-05-13 21:00:38 +0800

    Format code to control bird object
    Create bird_create_pattion method.

--- AppMain.cpp
+++ AppMain.cpp
@@ -3,7 +3,14 @@
 #include"SwimmingBird.h"
 #include "BirdFactory.h"
 
+void bird_create_pattion();
 void main()
+{
+	bird_create_pattion();
+}
+
+//Using simple factory pattern to create bird object
+void bird_create_pattion()
 {
 	FlyBird *b1 = (FlyBird*)BirdFactory::Create("DaYan", 100.0, FLY_BIRD);
 	b1->WhoAmI();
@@ -13,5 +20,5 @@ void main()
 	b2->WhoAmI();
 	b2->ICan();
 	system("PAUSE");
-			
+
 }
